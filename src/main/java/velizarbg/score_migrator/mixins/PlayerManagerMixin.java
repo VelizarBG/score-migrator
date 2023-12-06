3d19fb20886d342d38eaea0e0d45cd2d99826c77
Velizar <31567122+VelizarBG@users.noreply.github.com>
2023-12-07 00:47:35 +0200

    Update to 1.20.3/4
    - Also use MixinExtras bundled in new Fabric Loader

--- src/main/java/velizarbg/score_migrator/mixins/PlayerManagerMixin.java
+++ src/main/java/velizarbg/score_migrator/mixins/PlayerManagerMixin.java
@@ -1,48 +1,40 @@
 package velizarbg.score_migrator.mixins;
 
-import net.minecraft.network.ClientConnection;
-import net.minecraft.scoreboard.ScoreboardObjective;
-import net.minecraft.scoreboard.ScoreboardPlayerScore;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.scoreboard.ScoreAccess;
+import net.minecraft.scoreboard.ScoreHolder;
+import net.minecraft.scoreboard.ScoreboardScore;
 import net.minecraft.scoreboard.ServerScoreboard;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
-import net.minecraft.server.network.ConnectedClientData;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Map;
 
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
-	private static String cachedName;
-
 	@Shadow @Final private MinecraftServer server;
 
-	@ModifyVariable(method = "onPlayerConnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/UserCache;add(Lcom/mojang/authlib/GameProfile;)V"))
-	private String getCachedName(String string) {
-		cachedName = string;
-		return string;
-	}
-
-	@Inject(method = "onPlayerConnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;sendScoreboard(Lnet/minecraft/scoreboard/ServerScoreboard;Lnet/minecraft/server/network/ServerPlayerEntity;)V"))
-	private void migrateScores(ClientConnection connection, ServerPlayerEntity player, ConnectedClientData clientData, CallbackInfo ci) {
+	@SuppressWarnings("DataFlowIssue") // score can't be null
+	@Inject(method = "onPlayerConnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/UserCache;add(Lcom/mojang/authlib/GameProfile;)V"))
+	private void migrateScores(CallbackInfo ci, @Local ServerPlayerEntity player, @Local String cachedName) {
 		String newName = player.getGameProfile().getName();
-		if (server.getUserCache() == null || cachedName.equals(newName))
+		if (cachedName.equals(newName))
 			return;
 
 		ServerScoreboard scoreboard = server.getScoreboard();
-		Map<ScoreboardObjective, ScoreboardPlayerScore> playerScoresMap = scoreboard.getPlayerObjectives(cachedName);
-		if (!playerScoresMap.isEmpty()) {
-			for (Map.Entry<ScoreboardObjective, ScoreboardPlayerScore> entry : playerScoresMap.entrySet()) {
-				scoreboard.getPlayerScore(newName, entry.getKey()).setScore(entry.getValue().getScore());
-			}
-			scoreboard.resetPlayerScore(cachedName, null);
-		}
+		ScoreHolder scoreHolder = ScoreHolder.fromName(cachedName);
+		scoreboard.getScoreHolderObjectives(scoreHolder).forEach((objective, scoreInt) -> {
+			ScoreboardScore oldScore = (ScoreboardScore) scoreboard.getScore(scoreHolder, objective);
+			ScoreAccess newScore = scoreboard.getOrCreateScore(player, objective, true);
+			newScore.setDisplayText(oldScore.getDisplayText());
+			newScore.setNumberFormat(oldScore.getNumberFormat());
+			newScore.setScore(scoreInt);
+		});
+		scoreboard.removeScores(scoreHolder);
 	}
 }
